1c3372a3aad3a6830fa6033d2eb4adf9ecd815bc
banana <julien.travail@gmail.com>
2020-07-14 08:24:42 +0200

    Implement CRUD on back end

--- backEnd/app.js
+++ backEnd/app.js
@@ -1,22 +1,25 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
+const product = require('./routes/product.route'); // Imports routes for the products
 const app = express();
-app.set('views', __dirname + '/views');
 
-app.engine('html', require('ejs').renderFile);
+// Set up mongoose connection
+const mongoose = require('mongoose');
 
-app.use((req, res, next) => {
-  res.status(201);
-  next();
-});
+mongoose.connect('mongodb://localhost:27017',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false})
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+mongoose.Promise = global.Promise;
+let db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
-app.use((req, res, next) => {
-  res.render("index.html");
-  next();
-});
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 
-app.use((req, res, next) => {
-  console.log('Réponse envoyée avec succès !');
-});
+app.use('/products', product);
 
 module.exports = app;
